8bbdbe0e5a01bd7ae5b53d4996158c43d82b6b93
ykkz000 <ykkz000@163.com>
2026-07-22 21:38:36 +0800

    修正信息

--- src/client/java/top/ykkz000/doublejump/client/DoubleJumpClient.java
+++ src/client/java/top/ykkz000/doublejump/client/DoubleJumpClient.java
@@ -1,10 +1,12 @@
 package top.ykkz000.doublejump.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public class DoubleJumpClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 	}
 }
--- src/main/java/top/ykkz000/doublejump/DoubleJump.java
+++ src/main/java/top/ykkz000/doublejump/DoubleJump.java
@@ -6,27 +6,21 @@ import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.resources.Identifier;
 
+import org.jspecify.annotations.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DoubleJump implements ModInitializer {
 	public static final String MOD_ID = "double-jump";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 		AutoConfig.register(DoubleJumpConfig.class, GsonConfigSerializer::new);
-		LOGGER.info("Register mod config");
+		LOGGER.info("Registered mod config");
 	}
 
-	public static Identifier id(String path) {
+	public static Identifier id(@NonNull String path) {
 		return Identifier.fromNamespaceAndPath(MOD_ID, path);
 	}
 }
